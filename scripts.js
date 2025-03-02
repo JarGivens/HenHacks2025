a8a501435216429c05d7820345c5834a18e850ab
Jack <jgivens@udel.edu>
2025-03-01 20:04:49 -0500

    Implemented random problem generation

--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
 //Use this file to add interactability to your website.
-
-function checkAnswer(input){
-    if(answer = 4){
-        alert("You got it right");
-    } else {
-        alert("you got it wrong")
-    }
-}
