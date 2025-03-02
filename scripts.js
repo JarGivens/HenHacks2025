2827eac5c4323f59088538044f4b6fd457b42cf6
Jack <jgivens@udel.edu>
2025-03-01 19:35:14 -0500

    Implemented checking 2 + 2 (work in progress)

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 //Use this file to add interactability to your website.
 
+function checkAnswer(input){
+    if(answer = 4){
+        alert("You got it right");
+    } else {
+        alert("you got it wrong")
+    }
+}
